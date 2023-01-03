f5d3577297f9e924058a8fb007c7189639b228ae
Avery Ramirez <68864812+averyramirez@users.noreply.github.com>
2023-01-03 11:13:00 -0800

    Update app.js
    Removed to do list

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 //Date and time
 function display_ct7() {
-    var x = new Date()
-    var ampm = x.getHours( ) >= 12 ? ' PM' : ' AM';
-    hours = x.getHours( ) % 12;
-    hours = hours ? hours : 12;
-    hours=hours.toString().length==1? 0+hours.toString() : hours;
-    
-    var minutes=x.getMinutes().toString()
+  var x = new Date()
+  var ampm = x.getHours( ) >= 12 ? ' PM' : ' AM';
+  hours = x.getHours( ) % 12;
+  hours = hours ? hours : 12;
+  hours=hours.toString().length==1? 0+hours.toString() : hours;
+  
+  var minutes=x.getMinutes().toString()
     minutes=minutes.length==1 ? 0+minutes : minutes;
     
     var seconds=x.getSeconds().toString()
@@ -27,58 +27,4 @@ function display_ct7() {
     var refresh=1000; // Refresh rate in milli seconds
     mytime=setTimeout('display_ct7()',refresh)
     }
-    display_c7()
-
-
-//To-do list
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
-
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
-
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
-
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
-
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
-}
+    display_c7();
